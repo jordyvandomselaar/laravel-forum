92df02f5f5c4011110d38beb9320bd17ac9d4b34
Andreas Elia <andreaselia@live.co.uk>
2017-06-11 23:33:11 +0100

    Some basic inline styling

--- resources/views/discussions/show.blade.php
+++ resources/views/discussions/show.blade.php
@@ -3,19 +3,21 @@
 @section('content')
     <div class="panel panel-default">
         <div class="panel-heading">
-            {{ $discussion->title }}
+            <div class="panel-title pull-left">{{ $discussion->title }}</div>
 
-            <form action="{{ $discussion->locked_at ? route('forum.lock.destroy') : route('forum.lock.store') }}">
-                {{ csrf_field() }}
-                <input type="hidden" name="discussion_id" value="{{ $discussion->id }}">
-                <input type="submit" value="{{ $discussion->locked_at ? 'Unlock' : 'Lock' }}" class="btn btn-{{ $discussion->locked_at ? 'danger' : 'success' }} pull-right">
-            </form>
+            <div class="pull-right">
+                <form action="{{ $discussion->locked_at ? route('forum.lock.destroy') : route('forum.lock.store') }}" style="display: inline" style="margin-right: 5px">
+                    {{ csrf_field() }}
+                    <input type="hidden" name="discussion_id" value="{{ $discussion->id }}">
+                    <input type="submit" value="{{ $discussion->locked_at ? 'Unlock' : 'Lock' }}" class="btn btn-{{ $discussion->locked_at ? 'danger' : 'success' }} pull-right">
+                </form>
 
-            <form action="{{ $discussion->stickied_at ? route('forum.sticky.destroy') : route('forum.sticky.store') }}">
-                {{ csrf_field() }}
-                <input type="hidden" name="discussion_id" value="{{ $discussion->id }}">
-                <input type="submit" value="{{ $discussion->stickied_at ? 'Unsticky' : 'Sticky' }}" class="btn btn-{{ $discussion->locked_at ? 'danger' : 'success' }} pull-right">
-            </form>
+                <form action="{{ $discussion->stickied_at ? route('forum.sticky.destroy') : route('forum.sticky.store') }}" style="display: inline">
+                    {{ csrf_field() }}
+                    <input type="hidden" name="discussion_id" value="{{ $discussion->id }}">
+                    <input type="submit" value="{{ $discussion->stickied_at ? 'Unsticky' : 'Sticky' }}" class="btn btn-{{ $discussion->locked_at ? 'danger' : 'success' }} pull-right">
+                </form>
+            </div>
         </div>
 
         <div class="panel-body">
--- resources/views/partials/post.blade.php
+++ resources/views/partials/post.blade.php
@@ -1,6 +1,6 @@
 <li class="list-group-item">
-    <div class="row">
-        <div class="col-xs-12 col-sm-3">
+    <div class="row" style="display: flex">
+        <div class="col-xs-12 col-sm-3" style="border-right: 1px solid #ddd">
             {{ $post->user->name }}
         </div>
         <div class="col-xs-12 col-sm-9">
