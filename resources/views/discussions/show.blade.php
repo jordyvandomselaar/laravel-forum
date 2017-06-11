2e9f33582b8d9df2332828ac90129191a838538c
Andreas Elia <andreaselia@live.co.uk>
2017-06-11 13:01:51 +0100

    Fix button position

--- resources/views/discussions/show.blade.php
+++ resources/views/discussions/show.blade.php
@@ -8,13 +8,13 @@
             <form action="{{ $discussion->locked_at ? route('forum.lock.destroy') : route('forum.lock.store') }}">
                 {{ csrf_field() }}
                 <input type="hidden" name="discussion_id" value="{{ $discussion->id }}">
-                <input type="submit" value="{{ $discussion->locked_at ? 'Unlock' : 'Lock' }}" class="btn btn-{{ $discussion->locked_at ? 'danger' : 'success' }}">
+                <input type="submit" value="{{ $discussion->locked_at ? 'Unlock' : 'Lock' }}" class="btn btn-{{ $discussion->locked_at ? 'danger' : 'success' }} pull-right">
             </form>
 
             <form action="{{ $discussion->stickied_at ? route('forum.sticky.destroy') : route('forum.sticky.store') }}">
                 {{ csrf_field() }}
                 <input type="hidden" name="discussion_id" value="{{ $discussion->id }}">
-                <input type="submit" value="{{ $discussion->stickied_at ? 'Unsticky' : 'Sticky' }}" class="btn btn-{{ $discussion->locked_at ? 'danger' : 'success' }}">
+                <input type="submit" value="{{ $discussion->stickied_at ? 'Unsticky' : 'Sticky' }}" class="btn btn-{{ $discussion->locked_at ? 'danger' : 'success' }} pull-right">
             </form>
         </div>
 
