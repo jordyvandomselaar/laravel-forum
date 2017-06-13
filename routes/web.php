b0908f667e1eacb4d6613dd7c41c842efb7cd5e6
Andreas Elia <andreaselia@live.co.uk>
2017-06-13 23:25:45 +0100

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-Route::group(['prefix' => config('forum.prefix'), 'as' => 'forum.', 'namespace' => config('forum.namespace')], function () {
     Route::resource('groups', 'GroupController', ['store', 'update', 'destroy']);
     Route::resource('discussions', 'DiscussionController', ['store', 'update', 'destroy']);
     Route::resource('posts', 'PostController', ['store', 'update', 'destroy']);
