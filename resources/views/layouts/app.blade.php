f571280ee2bce02f3c43690e86d3a911d609387e
Andreas Elia <andreaselia@live.co.uk>
2017-06-14 20:54:14 +0100

    Add jQuery CDN

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -123,6 +123,7 @@
         @include('forum::partials.footer')
     </div>
 
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
     @yield('foot')
