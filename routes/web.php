5cda96ad2780e89570adda789c1c38184358b06a
Andreas Elia <andreaselia@live.co.uk>
2017-06-14 20:28:01 +0100

    Routes fix

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 Route::prefix(config('forum.prefix'))->namespace(config('forum.namespace'))->name('forum.')->group(function () {
+    Route::get('/', 'ForumController@index')->name('home');
+    Route::post('search', 'ForumController@search')->name('search');
+
     Route::resource('groups', 'GroupController', ['store', 'update', 'destroy']);
     Route::resource('discussions', 'DiscussionController', ['store', 'update', 'destroy']);
     Route::resource('posts', 'PostController', ['store', 'update', 'destroy']);
